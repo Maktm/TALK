b5c25db1c134c3e18540543dbe10e3bc1165d5e3
Michael Kiros <michaelakiros@gmail.com>
2019-10-20 15:17:38 -0500

    Adds the journal activity
    The activity is just the basic layout for now.

---
+++ app/src/main/java/com/computerscience/talk/JournalActivity.java
@@ -0,0 +1,14 @@
+package com.computerscience.talk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JournalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_journal);
+    }
+}
