73090e9c55543553e5fed6bd4893354b541b5eea
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-10-24 13:13:18 -0500

    hello world

--- app/src/main/java/com/computerscience/talk/MainMenuActivity.java
+++ app/src/main/java/com/computerscience/talk/MainMenuActivity.java
@@ -10,5 +10,39 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        
+        configuretoJournalButton();
+        configuretoResourcesButton();
+        configuretoGraphButton();
+    }
+    
+    private void configuretoJournalButton() {
+        Button toJournal = (Button) findViewById(R.id.toJournal);
+        toJournal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, JournalActivity.class));
+            }
+        });
+    }
+    
+    private void configuretoResourcesButton() {
+        Button toResources = (Button) findViewById(R.id.toResources);
+        toResources.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, ResourcesActivity.class));
+            }
+        });
+    }
+    
+    private void configuretoGraphButton() {
+        Button toGraph = (Button) findViewById(R.id.toGraph);
+        toGraph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, GraphActivity.class));
+            }
+        });
     }
 }
