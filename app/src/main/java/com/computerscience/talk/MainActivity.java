5d444ef96e71743271951f082da1d0898af64402
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-10-24 13:04:52 -0500

    I didn't test this

--- app/src/main/java/com/computerscience/talk/MainActivity.java
+++ app/src/main/java/com/computerscience/talk/MainActivity.java
@@ -10,5 +10,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        configureSadButton();
+        configureAnxiousButton();
+        configureAngryButton();
+        configureHappyButton();
+    }
+    
+    private void configureSadButton() {
+        Button sadButton = (Button) findViewById(R.id.sadButton);
+        sadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SliderActivity.class));
+            }
+        });
+    }
+    
+    private void configureAnxiousButton() {
+        Button anxiousButton = (Button) findViewById(R.id.anxiousButton);
+        anxiousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SliderActivity.class));
+            }
+        });
+    }
+    
+    private void configureAngryButton() {
+        Button angryButton = (Button) findViewById(R.id.angryButton);
+        angryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SliderActivity.class));
+            }
+        });
+    }
+    
+    private void configureHappyButton() {
+        Button happyButton = (Button) findViewById(R.id.happyButton);
+        happyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SliderActivity.class));
+            }
+        });
     }
 }
