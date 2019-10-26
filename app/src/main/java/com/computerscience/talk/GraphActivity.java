cbde35e1fcf39a8dfb7df250714199f3ddfaa6cd
Michael Kiros <michaelakiros@gmail.com>
2019-10-26 12:59:20 -0500

    Add graphing library with sample
    This commit introduces the graphing library along with a sample
    graph that draw a line that cuts through the diagonal. In the
    future, the data should be pulled from the SQLite database.

--- app/src/main/java/com/computerscience/talk/GraphActivity.java
+++ app/src/main/java/com/computerscience/talk/GraphActivity.java
@@ -3,9 +3,14 @@ package com.computerscience.talk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.view.View;
-import android.content.Intent;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GraphActivity extends AppCompatActivity {
 
@@ -13,5 +18,21 @@ public class GraphActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+
+        // Get the line chart object
+        LineChart chart = (LineChart)findViewById(R.id.chart);
+
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            entries.add(new Entry(i, i));
+        }
+
+        LineDataSet dataSet = new LineDataSet(entries, "Label");
+        dataSet.setCircleColor(50);
+        dataSet.setValueTextColor(100);
+
+        LineData lineData = new LineData(dataSet);
+        chart.setData(lineData);
+        chart.invalidate();
     }
 }
