2f8dea8958b6740f0a2e2ac7fb449a07f36ab5d0
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-11-05 13:30:29 -0600

    Update ResourcesActivity.java

--- app/src/main/java/com/computerscience/talk/ResourcesActivity.java
+++ app/src/main/java/com/computerscience/talk/ResourcesActivity.java
@@ -8,20 +8,22 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class ResourcesActivity extends AppCompatActivity {
-
-    //Demo Array
-    String[] demoResources = {"Activity 1", "Activity 2", "Activity 3", "Activity 4", "Activity 5"};
+public class ResourcesActivity extends Activity implements OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
 
-        ArrayList<String> arrayOfStuff = new ArrayList<>();
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_resources, arrayOfStuff);
-
-        ListView listView = (ListView) findViewById(R.id.mobile_list);
-        listView.setAdapter(adapter);
+        ListView listView = (ListView) findViewById(R.id.listViewR);
+        listView.setOnItemClickListener(this);
     }
+    
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+            Intent intent = new Intent();
+            intent.setClass(this, ListItemDetail.class);
+            intent.putExtra("position", position);
+            intent.putExtra("id", id);
+            startActivity(intent);
 }
