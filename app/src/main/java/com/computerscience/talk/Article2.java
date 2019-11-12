e6c2ef68473c59effeb1e34f3cf12686cfbc28ce
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-11-12 13:10:09 -0600

    Added the activities for all the resources
    I'll fill them with actual content later

---
+++ app/src/main/java/com/computerscience/talk/Article2.java
@@ -0,0 +1,14 @@
+package com.computerscience.talk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Article2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article2);
+    }
+}
---
+++ app/src/main/java/com/computerscience/talk/Article4.java
@@ -0,0 +1,14 @@
+package com.computerscience.talk;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Article4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article4);
+    }
+}
