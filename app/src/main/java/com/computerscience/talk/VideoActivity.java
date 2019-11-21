f5179f09f7b15c1f2a1e936cbba6e787ffec0747
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-11-21 13:11:10 -0600

    H
    Added the video player, should 100% work
    
    Still can't fix the errors in the activities tho

--- app/src/main/java/com/computerscience/talk/VideoActivity.java
+++ app/src/main/java/com/computerscience/talk/VideoActivity.java
@@ -3,12 +3,27 @@ package com.computerscience.talk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.VideoView;
+import android.widget.MediaController;
+import android.net.Uri;
 
 public class VideoActivity extends AppCompatActivity {
+
+    VideoView vid;
+    MediaController m;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
+
+        vid = (VideoView)findViewById(R.id.videoView);
+        m = new MediaController(this);
+
+        String path = "/Users/Owner/TALK/app/src/main/res/raw/Cats.mp4";
+
+        Uri u = Uri.parse(path);
+        vid.setVideoURI(u);
+        vid.start();
     }
 }
