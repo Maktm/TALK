0d2588a132dc09a819ae1efa9daf014d67da2a1d
ssuleiman <saeednadims@gmail.com>
2019-12-02 19:29:44 -0600

    Update JournalFragment.java

--- app/src/main/java/com/texastech/talk/navigation/JournalFragment.java
+++ app/src/main/java/com/texastech/talk/navigation/JournalFragment.java
@@ -16,8 +16,6 @@ import android.view.ViewGroup;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.texastech.talk.R;
 
-import.java.swing.*;
-
 
 /**
  * A simple {@link Fragment} subclass.
@@ -82,21 +80,21 @@ public class JournalFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 // TODO: Add code for creating a journal entry
-            public class TextArea{
-                TextArea(){
-                JFrame f = new JFrame();
-                JTextArea area = new JTextArea("How are you feeling?");
-                area.setBounds(10,30,200,200);
-                f.add(area);
-                f.setSize(300,300);
-                f.setLayout(null);
-                f.setVisible(true);
-                }
-            public static void main(String args[]){
-            new TextArea();
-            }}
-            }
-        });
+            <EditText
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:hint="How are you feeling?"
+                android:id="@+id/edit_text"
+                android:layout_marginTop="10dp"
+                android:inputType="textCapWords"/>
+                    
+             <TextView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:layout_centerHorizontal="true"
+                android:layoutcenter_Vertical="true"
+                android:text="@string/hello_world"
+                tools:context=".FrmActivity"/>
     }
 
     @Override
