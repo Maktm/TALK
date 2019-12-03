7e702688e3971be62db6989e6a18aa7d6b7c141e
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-12-02 18:48:07 -0600

    I did a bunch of stuff
    - Replaced ListView with CardView
    - Only CardView displaying sadness article is functioning atm, needs database additions

--- app/src/main/java/com/computerscience/talk/ResourcesActivity.java
+++ app/src/main/java/com/computerscience/talk/ResourcesActivity.java
@@ -14,60 +14,18 @@ import android.util.Log;
 
 import java.util.ArrayList;
 
-public class ResourcesActivity extends Activity implements OnItemClickListener {
+public class ResourcesActivity extends Activity {
     String mood = "Sad";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
-
-        ListView listView = (ListView) findViewById(R.id.listViewR);
-        listView.setOnItemClickListener(this);
     }
 
-    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
-        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
-        Intent inten = new Intent();
-        inten.setClass(ResourcesActivity.this, ListItemDetail.class);
-        inten.putExtra("position", position);
-        inten.putExtra("id", id);
-        if(mood.equals("sad") && id == 0) {
-            startActivity(new Intent(ResourcesActivity.this, Article1.class));
-        }
-        else if (mood.equals("sad") && id == 1) {
-            startActivity(new Intent(ResourcesActivity.this, Fitness1.class));
-        }
-        else if (mood.equals("sad") && id == 2) {
-            startActivity(new Intent(ResourcesActivity.this, VideoActivity.class));
-        }
-        else if (mood.equals("anxious") && id == 0) {
-            startActivity(new Intent(ResourcesActivity.this, Article2.class));
-        }
-        else if (mood.equals("anxious") && id == 1) {
-            startActivity(new Intent(ResourcesActivity.this, Fitness1.class));
-        }
-        else if (mood.equals("anxious") && id == 2) {
-            startActivity(new Intent(ResourcesActivity.this, VideoActivity.class));
-        }
-        else if (mood.equals("angry") && id == 0) {
-            startActivity(new Intent(ResourcesActivity.this, Article3.class));
-        }
-        else if (mood.equals("angry") && id == 1) {
-            startActivity(new Intent(ResourcesActivity.this, Fitness2.class));
-        }
-        else if (mood.equals("angry") && id == 2) {
-            startActivity(new Intent(ResourcesActivity.this, VideoActivity.class));
-        }
-        else if (mood.equals("happy") && id == 0) {
-            startActivity(new Intent(ResourcesActivity.this, Article4.class));
-        }
-        else if (mood.equals("happy") && id == 1) {
-            startActivity(new Intent(ResourcesActivity.this, Fitness2.class));
-        }
-        else {
-            startActivity(new Intent(ResourcesActivity.this, VideoActivity.class));
-        }
-}
+    public void openArticle(View view)
+    {
+        startActivity(new Intent(ResourcesActivity.this, Article1.class));
+    }
 
 }
