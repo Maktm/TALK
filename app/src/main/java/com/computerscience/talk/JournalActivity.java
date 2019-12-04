b317b79fdd610469b7e65ae65804b2c425f8eedd
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-12-03 23:03:00 -0600

    fgh
    Made a slightly better attempt at the journal database linking

--- app/src/main/java/com/computerscience/talk/JournalActivity.java
+++ app/src/main/java/com/computerscience/talk/JournalActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class JournalActivity extends AppCompatActivity {
+    String text;
+    int date = 1234;
+    String title = "Something here.";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +23,17 @@ public class JournalActivity extends AppCompatActivity {
         //Does stuff to make the entry
     }
 
+    EditText inputText = (EditText)findViewById(R.id.edit_text);
+
     public void saveEntry(View view)
     {
+        String text = inputText.getText().toString();
         /**There was an attempt.
          * This is code for when the charlotte-resources branch merges w the database branch:
          * What should happen is that there is a space in journal that allows users to enter text. The text needs to be saved to variables that
          * are then passed to this function, where the values are taken and added to the database. I have no idea how to do any of this.
          * date = 4754564
-         * text = "Dear diary..."; (Take the text as input for the function, ideally
-         * title = "Hello World"; (Also take this as input)
+         * text = inputText.getText().toString(); (Take the text as input)
          *
          * Journal journal = new Journal(date, text, title);
          * JournalDao journalDao = database.journalDao;
