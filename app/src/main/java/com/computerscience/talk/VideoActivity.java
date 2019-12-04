c2261afb4f25e811071c4f922664436dc2a81e50
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-12-03 22:28:54 -0600

    fixed the articles
    they look nice and presentable now

--- app/src/main/java/com/computerscience/talk/VideoActivity.java
+++ app/src/main/java/com/computerscience/talk/VideoActivity.java
@@ -2,28 +2,41 @@ package com.computerscience.talk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.view.View;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.VideoView;
 import android.widget.MediaController;
 import android.net.Uri;
+import android.graphics.PixelFormat;
 
 public class VideoActivity extends AppCompatActivity {
 
     VideoView vid;
     MediaController m;
+    MediaPlayer mediaPlayer;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    private SurfaceHolder getWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
+        getWindow.setFormat(PixelFormat.UNKNOWN);
+
         vid = (VideoView)findViewById(R.id.videoView);
         m = new MediaController(this);
 
-        String path = "android.resource://com.GitHub.TALK/"+R.raw.cats;
+        String path = "raw/cats.mp4";
 
         Uri u = Uri.parse(path);
         vid.setVideoURI(u);
+        vid.requestFocus();
         vid.start();
     }
 }
