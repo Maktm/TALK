7f1418a6f03e4580928d86e877f37fe4a9ce7890
CharlottePhillipss <47573675+CharlottePhillipss@users.noreply.github.com>
2019-12-03 22:44:54 -0600

    Update JournalActivity.java

--- app/src/main/java/com/computerscience/talk/JournalActivity.java
+++ app/src/main/java/com/computerscience/talk/JournalActivity.java
@@ -23,7 +23,9 @@ public class JournalActivity extends AppCompatActivity {
     {
         /**There was an attempt.
          * This is code for when the charlotte-resources branch merges w the database branch:
-         * date =
+         * What should happen is that there is a space in journal that allows users to enter text. The text needs to be saved to variables that
+         * are then passed to this function, where the values are taken and added to the database. I have no idea how to do any of this.
+         * date = 4754564
          * text = "Dear diary..."; (Take the text as input for the function, ideally
          * title = "Hello World"; (Also take this as input)
          *
